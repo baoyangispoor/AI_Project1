09093d2dc490b3b18d9223963371534684acb26c
baoyangispoor <bao.ya@northeastern.edu>
2021-10-02 21:22:52 -0700

    first commit

---
+++ AStart.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AStart {
+    List<Point> pointList=new ArrayList();//store the position,index and name for each node
+    List<String> matrix=new ArrayList();//store connectivity
+    // in the construction method, initialize the pointList and matrix
+    public AStart() {
+
+    }
+    // return a list of city to represent the path from city1 to city2 with A* algorithm
+    public List<String> findBestPath(String city1,String city2) {
+        return null;
+    }
+}
---
+++ Point.java
@@ -0,0 +1,38 @@
+public class Point {
+    int x;//x index
+    int y;//y index
+    int idx;// index in the list
+    String cityName;//name of city
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getIdx() {
+        return idx;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setIdx(int idx) {
+        this.idx = idx;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
